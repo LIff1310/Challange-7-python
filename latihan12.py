c70fced18dfdfffeb7436dbc3a33ad2ab6453021
Alif <firjatullahalif1310@gmail.com>
2024-03-06 16:24:37 +0700

    first commit

---
+++ latihan12.py
@@ -0,0 +1,10 @@
+print("LATIHAN 12")
+
+data = ["Do", "Re", "Mi", "Fa", "Sol", "La", "Si"]
+print(data)
+data.remove("Re")
+data.append("Re")
+data.remove("Mi")
+data.insert(0, "Mi")
+print("data sesudah : ")
+print(data)
